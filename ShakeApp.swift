a95252c9d2f502ab16f32fb360599dbd84bc115b
ichi <realkouu@gmail.com>
2026-02-06 10:29:04 +0900

    Initial commit

---
+++ ShakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShakeApp.swift
+//  Shake
+//
+//  Created by 福田光一郎 on 2026/02/06.
+//
+
+import SwiftUI
+
+@main
+struct ShakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
